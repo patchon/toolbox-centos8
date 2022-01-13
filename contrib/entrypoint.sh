de1690f030e5f518f84d405d50453b780e5aea82
patchon <martinsson.patrik@gmail.com>
2022-01-13 11:35:29 +0100

    fixing pwd line

--- contrib/entrypoint.sh
+++ contrib/entrypoint.sh
@@ -2,11 +2,10 @@
 
 uid=$(id -u)
 
-if [[ "${uid}" != "0" && "${uid}" != x"1001" ]]; then
+if [[ "${uid}" != "0" ]]; then
   NSS_WRAPPER_PASSWD=/tmp/passwd.nss_wrapper
   NSS_WRAPPER_GROUP=/etc/group
-  #cat /etc/passwd | sed -e 's/^ftp:/builder:/' > ${wrapper_passwd}
-  echo "build:x:${uid}:0:builder,,,:/tmp/:/bin/bash" >> ${NSS_WRAPPER_PASSWD}
+  echo "toolbox:x:${uid}:0:toolbox:/tmp:/bin/bash" > ${NSS_WRAPPER_PASSWD}
   export NSS_WRAPPER_PASSWD
   export NSS_WRAPPER_GROUP
   LD_PRELOAD=/usr/local/lib64/libnss_wrapper.so
